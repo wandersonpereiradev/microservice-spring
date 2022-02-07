b90abdf756e45202b161889831610271fb9779c3
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2022-02-07 12:25:21 -0300

    hr-payroll implementation with mock

---
+++ hr-payroll/src/main/java/com/wandev/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.wandev.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.wandev.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(Long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
