20a85799cee59acbf29016ca4842b1828dad682f
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2022-02-09 20:23:42 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/wandev/hruser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.wandev.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wandev.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+
+}
