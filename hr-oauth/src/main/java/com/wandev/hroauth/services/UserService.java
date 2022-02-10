74ec800b509a43b59b91603999cd82020b6b47e2
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2022-02-10 10:53:41 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/wandev/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/wandev/hroauth/services/UserService.java
@@ -3,6 +3,9 @@ package com.wandev.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.wandev.hroauth.entities.User;
@@ -10,7 +13,7 @@ import com.wandev.hroauth.feignclients.UserFeignClient;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 
